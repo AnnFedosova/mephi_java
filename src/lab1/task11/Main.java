ff37b2180ba83f136889785fc1ca0f4fc8f79ab6
Anna <AnnFedosova@yandex.ru>
2018-02-15 11:56:12 +0300

    we have 11 tasks

--- src/lab1/task11/Main.java
+++ src/lab1/task11/Main.java
@@ -1,5 +1,9 @@
 package lab1.task11;
 
+/**
+ * Write a program that reads a line of text and prints all characters that are not ASCII,
+ * together with their Unicode values.
+ */
 public class Main {
     public static void main(String[] args) {
 
