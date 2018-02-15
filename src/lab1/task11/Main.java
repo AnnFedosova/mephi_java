f680796a08b5eaa4cf1ab0453de720ae837035b2
Anna <AnnFedosova@yandex.ru>
2018-02-15 19:44:29 +0300

    we have 11 tasks

--- src/lab1/task11/Main.java
+++ src/lab1/task11/Main.java
@@ -1,11 +1,15 @@
 package lab1.task11;
 
+import java.util.Scanner;
 /**
  * Write a program that reads a line of text and prints all characters that are not ASCII,
- * together with their Unicode values.
+ * together with their Unicode values. Пример: ĕ ¬>>>>>
  */
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter line: ");
+        String str = sc.nextLine();
+        System.out.println(str.replaceAll("\\p{ASCII}",""));
     }
 }
