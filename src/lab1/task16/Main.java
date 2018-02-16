a9995a755a1a8349c2f6f6031f33a9ead6472942
Anna <AnnFedosova@yandex.ru>
2018-02-17 00:56:40 +0300

    We have 16 tasks

--- src/lab1/task16/Main.java
+++ src/lab1/task16/Main.java
@@ -1,18 +1,17 @@
 package lab1.task16;
 
-import java.util.Scanner;
-
 /**
  * Improve the average method so that it is called with at least one parameter.
  */
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number: ");
-        int a = sc.nextInt();
-        System.out.println("Enter second number: ");
-        int b = sc.nextInt();
-        System.out.println("Average: " + average(a,b));
+        System.out.println("Average: " + average(1,2,3 ));
     }
-    private static double average(float x, float y) {return (x + y) / 2;}
+    //private static float average(float x, float y) {return (x + y) / 2;}
+    private static float average(float... values) {
+        float sum = 0;
+        for (float v : values)
+            sum += v;
+        return values.length == 0 ? 0 : sum / values.length;
+    }
 }
