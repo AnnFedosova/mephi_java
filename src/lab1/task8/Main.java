2706a644ff0bdd03795c3d1bc1390a1a40e6c7b6
Anna <AnnFedosova@yandex.ru>
2018-02-17 11:06:52 +0300

    We have 16 tasks

--- src/lab1/task8/Main.java
+++ src/lab1/task8/Main.java
@@ -1,7 +1,6 @@
 package lab1.task8;
 
 import java.util.Scanner;
-
 /**
  * Write a program that reads a string and prints all of its nonempty substrings.
  */
@@ -9,7 +8,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str =  sc.nextLine();
-        while (str.contains("  "))
+        System.out.println(str);
+        /*while (str.contains("  "))
             str = str.replace("  "," ");
         if (str.startsWith(" "))
             str = str.substring(1);
@@ -18,5 +18,12 @@ public class Main {
         System.out.println(str.replace(" " , ""));         //печать без пробелов
         for(int i = 0; i < (str.split(" ")).length; i++)              //печать отдельных слов
             System.out.println((str.split(" "))[i]);
+            */
+        //String[] arr = str.split(" ");
+        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j <= len; j++)
+                System.out.println(str.substring(i, j));
+        }
     }
 }
