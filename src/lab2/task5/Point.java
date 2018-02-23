122a000bd3de7e7297b449f7a19d70e1751132fd
Anna <AnnFedosova@yandex.ru>
2018-02-23 22:37:35 +0300

    2 lab, 5 task

---
+++ src/lab2/task5/Point.java
@@ -0,0 +1,33 @@
+package lab2.task5;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(){
+        this.x = 0;
+        this.y = 0;
+    }
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX(){ return x;}
+    public double getY(){ return y;}
+
+    public Point translate(double a, double b){
+        this.x += a;
+        this.y += b;
+        return this;
+    }
+    public Point scale(double a){
+        this.x = this.x * a;
+        this.y = this.y * a;
+        return this;
+    }
+    public void printPoint(){
+        System.out.println("X: " + getX());
+        System.out.println("Y: " + getY());
+    }
+}
