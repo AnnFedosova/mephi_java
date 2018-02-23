2bd44bd00abe1a7ceabfd99560275260e412b11a
Anna <AnnFedosova@yandex.ru>
2018-02-24 00:46:09 +0300

    2 lab, 8 task

---
+++ src/lab2/task8/Main.java
@@ -0,0 +1,36 @@
+package lab2.task8;
+
+/**
+ * In the preceding exercises, providing the constructors and getter methods of the Point class was rather repetitive.
+ * Most IDEs provide shortcuts for writing the boilerplate code. What does your IDE offer?
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass(7);
+    }
+    public static class MyClass{
+        int a;
+        int b;
+
+        public int getA() {
+            return a;
+        }
+
+        public int getB() {
+            return b;
+        }
+
+        public void setA(int a) {
+            this.a = a;
+        }
+
+        public void setB(int b) {
+            this.b = b;
+        }
+
+        public MyClass(int a) {
+            this.a = a;
+        }
+
+    }
+}
