ca21c843327877af9aaf94a0523556deb5371c6f
Anna <AnnFedosova@yandex.ru>
2018-02-23 17:58:39 +0300

    2 lab, 2 task

--- src/lab2/task2/Main.java
+++ src/lab2/task2/Main.java
@@ -1,6 +1,20 @@
 package lab2.task2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Consider the nextInt method of the Scanner class.
+ * Is it an accessor or mutator?
+ * Why? What about the nextInt method of the Random class?
+ */
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();       //метод nextInt() - аксессор тк не меняет состояние класса sc (это как get)
+        Random random = new Random();
+        int b = random.nextInt();   //аксессор тк не меняет состояние класса random
+        System.out.println(String.valueOf(a) + " " + String.valueOf(b));
     }
 }
