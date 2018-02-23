0b716f22e5b3490bb79ca157db5f8df4d3939138
Anna <AnnFedosova@yandex.ru>
2018-02-23 23:02:06 +0300

    2 lab, 6 task

--- src/lab2/task6/Main.java
+++ src/lab2/task6/Main.java
@@ -1,7 +1,12 @@
 package lab2.task6;
 
 /**
- *
+ * Repeat the preceding exercise, but now make translate and scale into mutators.
  */
 public class Main {
+    public static void main(String[] args) {
+        Point p = new Point(3, 4).translate(1, 3).scale(0.5);
+        //should set p to a point with coordinates (2, 3.5).
+        p.printPoint();
+    }
 }
