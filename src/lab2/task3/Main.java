142d20227af57ff5035b1f41e7b23689ceaf5b1c
Anna <AnnFedosova@yandex.ru>
2018-02-26 21:47:22 +0300

    everyth. 2 lab

--- src/lab2/task3/Main.java
+++ src/lab2/task3/Main.java
@@ -1,7 +1,7 @@
 package lab2.task3;
 
 import java.util.ArrayList;
-
+//import java.util.ArrayList;
 /**
  *  Can you ever have a mutator method return something other than void?    => YES
  *  Can you ever have an accessor method return void? Give examples when possible.
@@ -13,9 +13,30 @@ public class Main {
         if(list.add("Cat"))
             System.out.println("Mutator return boolean (now it's true)");
         System.out.println("List: " + list);
-        //list.get(0);
+
+        //Can you ever have an accessor method return void? Give examples when possible.
+        // можно получать данные не на выход функции как return, а в некоторую переменную
+        Accessor accessor = new Accessor();
+        accessor.printXY();
+        accessor.getXinY();
+        accessor.printXY();
     }
-    //Can you ever have an accessor method return void? Give examples when possible.
 }
 
+class Accessor{
+    private int x;  //from here
+    private int y;  //to here;
+
+    public Accessor(){
+        x = 12;
+        y = 0;
+    }
+    public void getXinY(){
+        y = x;
+    }
+    public void printXY(){
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+    }
+}
 
