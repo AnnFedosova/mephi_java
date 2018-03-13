d466b51d5eacf506d853c5437161cd2372325ff2
Anna <AnnFedosova@yandex.ru>
2018-03-13 17:14:47 +0300

    3 laba, no 3 task, no Extra in 4 task

---
+++ src/lab3/task4/Main.java
@@ -0,0 +1,13 @@
+package lab3.task4;
+
+/**
+ * Implement a static of method of the IntSequence class that yields a sequence with the arguments.
+ * For example, IntSequence.of(3, 1, 4, 1, 5, 9) yields a sequence with six values.
+ * Extra credit if you return an instance of an anonymous inner class.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(IntSequence.of(3, 1, 4, 1, 5, 9));
+    }
+}
+
