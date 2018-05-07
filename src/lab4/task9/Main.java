97d1a3b0b0e74686629236ca5edf8d4c9403af21
Anna <AnnFedosova@yandex.ru>
2018-05-07 14:07:58 +0300

    lab 4

--- src/lab4/task9/Main.java
+++ src/lab4/task9/Main.java
@@ -3,7 +3,7 @@ package lab4.task9;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
-/**todo
+/**todo спросить у Жени
  * Write a “universal” toString method that uses reflection to yield a string with all instance variables of an object.
  * Extra credit if you can handle cyclic references.
  *
@@ -14,17 +14,12 @@ import java.lang.reflect.Modifier;
 public class Main {
     private static Object startPoint = null;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         LabeledPoint a = new LabeledPoint(1, 2, "a",null);
         LabeledPoint b = new LabeledPoint(1,2,"b",a);
         LabeledPoint c = new LabeledPoint(1,2,"c",b);
         a.setPoint(c);
-        try {
-            System.out.println(Main.toString(a));
-        } catch (IllegalAccessException e) {
-            System.out.println("Can't print this object :(");
-            e.printStackTrace();
-        }
+        System.out.println(Main.toString(a));
     }
 
     public static String toString(Object object) throws IllegalAccessException {
