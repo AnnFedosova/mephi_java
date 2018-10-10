54de3b455f6741c57794cebd63cc67fd0b611a42
AnnFedosova <AnnFedosova@yandex.ru>
2018-10-10 19:34:31 +0300

    have task 6, 7 +-

---
+++ src/lab5/task6/Main.java
@@ -0,0 +1,49 @@
+package lab5.task6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * В разделе 5.1.6 приведен пример ошибочного внедрения блока операторов catch и finally
+ * в блок кода оператора try. Исправьте этот код,
+ * во-первых, перехватив исключение в операторе finally,
+ * во-вторых, внедрив блок операторов try/finally в блок операторов try / catch,
+ * и в-третьих, применив оператор try с ресурсами вместе с оператором catch.    +
+ */
+public class Main {
+    public static void main(String[] args) {
+        Path path = Paths.get("C:\\Development\\mephi_java\\src\\lab5\\task6\\test");
+        try (BufferedReader in = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+            //Читать из стандартного потока ввода in
+        } catch (IOException ex) {
+            System.err.println("Caught IOException: " + ex.getMessage());
+        }
+    }
+}
+
+
+
+
+
+
+
+/* В разделе 5.1.6 приведен пример:
+public class Main {
+    public static void main(String[] args) {
+        Path path = Paths.get("C:\\Development\\mephi_java\\src\\lab5\\task6\\test");
+        BufferedReader in = null;
+        try {
+            in = Files.newBufferedReader(path, StandardCharsets.UTF_8); //Читать из стандартного потока ввода in
+        } catch (IOException ex){
+            System.err.println("Caught IOException: " + ex.getMessage());
+        } finally {
+            if (in != null) {
+                in.close(); // ВНИМАНИЕ: может быть сгенерировано исключение! } }
+            }
+        }
+    }
+}*/
---
+++ src/lab5/task7/Main.java
@@ -0,0 +1,25 @@
+package lab5.task7;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Для выполнения этого упражнения вам придется просмотреть исходный код класса java.util.Scanner.
+ * Если при использовании класса Scanner операция ввода завершается неудачно,
+ * в этом классе перехватывается исключение в связи с ошибками ввода данных и закрывается ресурс,
+ * из которого осуществляется ввод.
+ * Что произойдет, если при закрытии ресурса будет сгенерировано исключение?
+ * Каким образом эта реализация взаимодействует с обработкой подавляемых исключений в операторе try с ресурсами?
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            Scanner scanner = new Scanner(new File("C:\\Development\\mephi_java\\src\\lab5\\task7\\testi"));
+        } catch (FileNotFoundException e) {
+            System.err.println("We have some problem: " + e.getMessage());
+        }
+
+    }
+}
