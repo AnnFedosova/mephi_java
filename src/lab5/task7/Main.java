81bdd229865b1590497b00177ec91e8af569573f
AnnFedosova <AnnFedosova@yandex.ru>
2018-10-25 22:30:02 +0300

    есть доп вопросы. сдала

--- src/lab5/task7/Main.java
+++ src/lab5/task7/Main.java
@@ -1,8 +1,6 @@
 package lab5.task7;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -18,8 +16,12 @@ public class Main {
         Exception e1 = new Exception(" hello");
 
         //todo проверить выполнение нижних двух строк.
+        //из приватного метода выкинуть e1
         Exception e2 = new Exception("msg"+e1.getMessage());
         Exception e3 = new Exception("msg",e1);
+
+        //getExampleException(e1);
+
 
         //throw e2;
         // будет  Exception in thread "main" java.lang.Exception: msg hello
@@ -29,44 +31,82 @@ public class Main {
         //Caused by: java.lang.Exception:  hello
 
         //todo из одного файла поток в другой файл. 1 try с ресурсами. 2 try finally
-        method2();
-
+        method1();
 
     }
 
     // try с ресурсами
-    private static void method1() {
+    private static void method1() throws Exception {
+        Exception exception = null;
+
         try (
-                Scanner scanner =
-                        new Scanner(new File("C:\\Development\\mephi_java\\src\\lab5\\task7\\inFile"));
+                NewScanner newScanner =
+                        new NewScanner(new File("C:\\Development\\mephi_java\\src\\lab5\\task7\\inFile"));
                 PrintWriter printWriter =
                         new PrintWriter(new File("C:\\Development\\mephi_java\\src\\lab5\\task7\\outFile"))
         ) {
-            while (scanner.hasNextLine())
-                printWriter.write(scanner.nextLine() + "\n");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+              printWriter.write(newScanner.nextLine() + "\n");
+        }catch (Exception e) {
+                exception = e;
+                e.printStackTrace();
         }
     }
 
     //try finally
-    private static void method2() {
-        Scanner scanner = null;
+    private static void method2() throws Exception {
+        NewScanner scanner = null;
         PrintWriter printWriter = null;
+        Exception exception = null;
         try {
             scanner =
-                    new Scanner(new File("C:\\Development\\mephi_java\\src\\lab5\\task7\\inFile"));
+                    new NewScanner(new File("C:\\Development\\mephi_java\\src\\lab5\\task7\\inFile"));
             printWriter =
                     new PrintWriter(new File("C:\\Development\\mephi_java\\src\\lab5\\task7\\outFile"));
-            while (scanner.hasNextLine())
                 printWriter.write(scanner.nextLine() + "\n");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }finally {
-            if(scanner != null)
-                scanner.close();
-            if (printWriter != null)
-                printWriter.close();
+        } catch (Exception e) {
+            exception = e;
+        } finally {
+            try {
+                if(scanner != null){
+                    scanner.close();
+                    System.out.println("Закрыли scanner");
+                }
+            }catch (Exception e){
+                if (exception != null){
+                    exception.addSuppressed(e);
+                }else {
+                    exception = e;
+                }
+            }
+            if (exception != null){
+                throw exception;
+            }
+
         }
     }
+
+
+    private static void getExampleException(Exception e1) throws Exception {
+        //throw new IOException("This is ExampleException");
+        throw e1;
+    }
 }
+
+class NewScanner implements AutoCloseable {
+    @Override
+    public void close() {
+        throw new RuntimeException("Я не могу закрыться");
+    }
+
+    public NewScanner(File source) throws FileNotFoundException {
+        File file = new File(source.getPath());
+
+        //throw new FileNotFoundException();
+    }
+
+    String nextLine() throws Exception {
+        throw new RuntimeException("from nextLine exception");
+        //return "from nextLine";
+    }
+}
+
