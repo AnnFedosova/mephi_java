38155b58a652db90892f787e03270fc54ea0c0f9
AnnFedosova <AnnFedosova@yandex.ru>
2018-12-07 14:26:57 +0300

    5 есть

--- src/lab6/task5/Main.java
+++ src/lab6/task5/Main.java
@@ -1,7 +1,9 @@
 package lab6.task5;
 
+import java.util.Arrays;
+
 /**
- * Рассмотрите следующий вариант метода swap(), где массив может быть предоставлен с помощью ар1ументов переменной длины:
+ * Рассмотрите следующий вариант метода swap(), где массив может быть предоставлен с помощью аргументов переменной длины:
  * public static <Т> Т[] swap(int i, int j, T... values) { T temp = values[i];
  * values[i] = values [j]; values[j] = temp; return values;
  * А теперь рассмотрите следующий вызов:
@@ -11,4 +13,30 @@ package lab6.task5;
  * Изменилось ли к лучшему сообщение об ошибке? Что нужно сделать для устранения ошибки?
  */
 public class Main {
+    public static void main(String[] args) {
+        //Double result = Main.swap(0, 1, 1.5, 2, 3);
+        //Какое сообщение об ошибке вы получите?
+        //Error:(17, 34) java: incompatible types:
+        // no instance(s) of type variable(s) T exist so that T[] conforms to java.lang.Double
+        //нет экземпляра (ов) типа (ов) типа T
+
+        //Double result2 = Main.<Double>swap(0, 1, 1.5, 2.0, 3.0);
+        //Изменилось ли к лучшему сообщение об ошибке? нет
+        //Error:(23, 43) java: incompatible types:
+        // java.lang.Double[] cannot be converted to java.lang.Double
+
+        Double[] result2 = Main.<Double>swap(0, 1, 1.5, 2.0, 3.0);
+
+
+    }
+
+    //@SafeVarargs
+    public static <T> T[] swap(int i, int j, T... values) {
+        T temp = values[i];
+        values[i] = values[j];
+        values[j] = temp;
+        return values;
+    }
+
+
 }
